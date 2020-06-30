9019eda633fb4519e830ae730fbf94c1a3a098f3
Anton Nesterov <antonnesterov939@gmail.com>
2020-06-30 11:46:17 +0300

    Updates

---
+++ precache-manifest.87eaeb449d312072b3f0d1595eab2a70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd800d19efef39dccf1ca4147ffb68b5",
+    "url": "/github_new_site/index.html"
+  },
+  {
+    "revision": "b9ea0cfb4f4357fb433d",
+    "url": "/github_new_site/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ee8c09ed55812d29017",
+    "url": "/github_new_site/static/css/main.b53f27ef.chunk.css"
+  },
+  {
+    "revision": "b9ea0cfb4f4357fb433d",
+    "url": "/github_new_site/static/js/2.3d1dd240.chunk.js"
+  },
+  {
+    "revision": "e7c69aa4549a0938e155d41214f1973f",
+    "url": "/github_new_site/static/js/2.3d1dd240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee8c09ed55812d29017",
+    "url": "/github_new_site/static/js/main.c49bd9bf.chunk.js"
+  },
+  {
+    "revision": "7c555c5cb859bb3a96b7",
+    "url": "/github_new_site/static/js/runtime-main.d514377d.js"
+  },
+  {
+    "revision": "75f5baf3c9d5d2de963add0bc1b95035",
+    "url": "/github_new_site/static/media/netflix.75f5baf3.png"
+  }
+]);
